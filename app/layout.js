f01d8c03027fb9f77a4be1d55b71139e4c6872f0
anmolag23 <anmolag232002@gmail.com>
2025-10-28 13:44:27 +0530

    initial setup

--- app/layout.js
+++ app/layout.js
@@ -1,15 +1,9 @@
-import { Geist, Geist_Mono } from "next/font/google";
+import { Inter } from "next/font/google";
 import "./globals.css";
+import { ThemeProvider } from "@/components/theme-provider";
+import { ConvexClientProvider } from "./ConvexClientProvider";
 
-const geistSans = Geist({
-  variable: "--font-geist-sans",
-  subsets: ["latin"],
-});
-
-const geistMono = Geist_Mono({
-  variable: "--font-geist-mono",
-  subsets: ["latin"],
-});
+const inter = Inter({subsets: ["latin"]});
 
 export const metadata = {
   title: "Create Next App",
@@ -18,11 +12,23 @@ export const metadata = {
 
 export default function RootLayout({ children }) {
   return (
-    <html lang="en">
+    <html lang="en" suppressHydrationWarning>
       <body
-        className={`${geistSans.variable} ${geistMono.variable} antialiased`}
+        className={`${inter.className}`}
       >
-        {children}
+        <ThemeProvider
+           attribute = "class"
+           defaultTheme = "dark"
+           enableSystem
+           disableTransitionOnChange
+           >
+          <ConvexClientProvider>
+          <main className="bg-slate-900 min-h-screen text-white overflow-x-hidden">
+             {children}
+          </main>  
+          </ConvexClientProvider>
+       
+        </ThemeProvider>
       </body>
     </html>
   );
