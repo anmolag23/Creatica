c03e0b0eccc8d6328c85a0877914dcb022608a94
anmolag23 <anmolag232002@gmail.com>
2025-10-29 01:11:17 +0530

    setup authentication using clerk

--- app/layout.js
+++ app/layout.js
@@ -2,6 +2,10 @@ import { Inter } from "next/font/google";
 import "./globals.css";
 import { ThemeProvider } from "@/components/theme-provider";
 import { ConvexClientProvider } from "./ConvexClientProvider";
+// import { ClerkProvider } from "@clerk/clerk-react";
+import { ClerkProvider } from "@clerk/nextjs";
+import { shadesOfPurple } from "@clerk/themes";
+import Header from "@/components/header";
 
 const inter = Inter({subsets: ["latin"]});
 
@@ -22,11 +26,19 @@ export default function RootLayout({ children }) {
            enableSystem
            disableTransitionOnChange
            >
+
+         <ClerkProvider
+           appearance={{
+             baseTheme: shadesOfPurple,
+           }}
+         >   
           <ConvexClientProvider>
+            <Header/>
           <main className="bg-slate-900 min-h-screen text-white overflow-x-hidden">
              {children}
           </main>  
           </ConvexClientProvider>
+          </ClerkProvider>
        
         </ThemeProvider>
       </body>
---
+++ convex/schema.js
@@ -0,0 +1,17 @@
+import {defineTable, defineSchema} from "convex/server";
+import { v } from "convex/values";
+
+export default defineSchema({
+    users: defineTable({
+  name: v.string(),
+  email: v.string(),
+  tokenIdentifier: v.string(),
+  imageUrl: v.optional(v.string()),
+  username: v.optional(v.string()),
+
+  createdAt: v.number(),
+  lastActiveAt: v.number(),
+}).index("by_token", ["tokenIdentifier"])
+  .index("by_email", ["email"])
+  .index("by_username", ["username"])
+})
