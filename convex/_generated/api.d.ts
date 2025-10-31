88406f5df3130c31efbe76ad6046f1b94500d3f1
anmolag23 <anmolag232002@gmail.com>
2025-10-31 12:29:08 +0530

    published Post page

--- convex/_generated/api.d.ts
+++ convex/_generated/api.d.ts
@@ -8,6 +8,9 @@
  * @module
  */
 
+import type * as comments from "../comments.js";
+import type * as follows from "../follows.js";
+import type * as likes from "../likes.js";
 import type * as posts from "../posts.js";
 import type * as public_ from "../public.js";
 import type * as users from "../users.js";
@@ -27,6 +30,9 @@ import type {
  * ```
  */
 declare const fullApi: ApiFromModules<{
+  comments: typeof comments;
+  follows: typeof follows;
+  likes: typeof likes;
   posts: typeof posts;
   public: typeof public_;
   users: typeof users;
